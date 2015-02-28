edbcfcfd48f4b2e8be74587d99e249a8ac21568b
Cass May <cass@cassm.net>
2015-02-28 20:34:50 +0000

    Add translation functionality and support for long strings to parrot, include goslate library

--- py/talkbox.py
+++ py/talkbox.py
@@ -1,6 +1,8 @@
 from dataplicity.client.task import Task, onsignal
 import urllib, urllib2, pycurl, subprocess
 import pygame
+from pygame.mixer import music
+import goslate
 import os
 import glob
 SONG_END = pygame.USEREVENT + 1
@@ -11,26 +13,25 @@ class Player(Task):
     trackNumber = 0
     playlistPath = ""
 
-
     def play_track(self):
         if self.trackNumber < len(self.songs):
             print "Playing track {} ({}).".format(self.trackNumber + 1, self.songs[self.trackNumber])
-            pygame.mixer.music.load(os.path.join(self.playlistPath, self.songs[self.trackNumber]))
-            pygame.mixer.music.play()
+            music.load(os.path.join(self.playlistPath, self.songs[self.trackNumber]))
+            music.play()
             
     def pre_startup(self):
         pygame.init()
         pygame.mixer.init()
-        pygame.mixer.music.set_endevent(SONG_END)
-        endtype = pygame.mixer.music.get_endevent() 
-        print "endevent type == {}: {}".format(endtype, endtype == SONG_END)
+        music.set_endevent(SONG_END)
+        endtype = music.get_endevent() 
+
     @onsignal('settings_update', 'player')
     def on_settings_update(self, name, settings):        
         action  = settings.get_list('controls', 'action')[0]
         
         if action == 'play':
-            if pygame.mixer.music.get_busy():
-                pygame.mixer.music.unpause()
+            if music.get_busy():
+                music.unpause()
             else:
                 inputPath = settings.get_list('path', 'path')[0]
                 self.playlistPath = inputPath
@@ -39,14 +40,14 @@ class Player(Task):
                     if ".mp3" in songName:
                         self.songs.append(songName)
                 self.songs.sort()
-                self.trackNumber = 2
+                self.trackNumber = 0
                 self.play_track()
                 
         elif action == 'pause':
-            pygame.mixer.music.pause()
+            music.pause()
 
         elif action == 'stop':
-            pygame.mixer.music.stop()
+            music.stop()
 
     def poll(self):
         """Called on a schedule defined in dataplicity.conf"""
@@ -62,6 +63,7 @@ class Player(Task):
 class Parrot(Task):
     """Samples a sin wave"""
     lastPhrase = "Test text"
+    lang = "en"
 
     def downloadFile(self, url, fileName):
         fp = open(filename, "wb")
@@ -72,16 +74,39 @@ class Parrot(Task):
         curl.close()
         fp.close()
 
-    def getGoogleSpeechURL(self, phrase):
-        googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
-        parameters = {'q': phrase}
-        data = urllib.urlencode(parameters)
-        googleTranslateURL = "%s%s" % (googleTranslateURL, data)
-        return googleTranslateURL
+    def translatePhrase(self, phrase, lang):
+        gs = goslate.Goslate()
+        phrase = gs.translate(phrase, lang)
+        return phrase
+
+    def splitPhrase(self, phrase, length):
+        phraseList = []
+        while len(phrase) > length:
+            phrasePart = phrase[:length].rsplit(' ', 1)[0]
+            phraseList.append(phrasePart)
+            phrase = phrase[len(phrasePart):]
+            if phrase[0] == ' ':
+                phrase = phrase[1:]
+        phraseList.append(phrase)
+        return phraseList
+            
+    def getGoogleSpeechURL(self, phraseSet, lang):
+        urlSet = []
+        for phrase in phraseSet:
+            encodedPhrase = urllib.quote(phrase.encode('utf8'))          
+            googleTranslateURL = "http://translate.google.com/translate_tts?ie=UTF-8&"
+            parameters = {'q': encodedPhrase, 'tl': lang}
+            data = urllib.urlencode(parameters)
+            googleTranslateURL = "%s%s" % (googleTranslateURL, data)
+            urlSet.append(googleTranslateURL)
+            print googleTranslateURL
+        return urlSet
 
     def speakText(self):
-        googleSpeechURL = self.getGoogleSpeechURL(self.phrase)
-        subprocess.call(["mplayer", googleSpeechURL], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        translatedPhrase = self.translatePhrase(self.phrase, self.lang)
+        phraseSet = self.splitPhrase(translatedPhrase, 100)
+        urlSet = self.getGoogleSpeechURL(phraseSet, self.lang)
+        subprocess.call(["mplayer"] + urlSet, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def pre_startup(self):
         """Called prior to running the project"""
@@ -95,9 +120,9 @@ class Parrot(Task):
     def on_settings_update(self, name, settings):
         # This signal is sent on startup and whenever settings are changed by the server
         self.phrase = settings.get_list('parrot', 'phrase')[0]
-        if self.phrase != self.lastPhrase:
-            self.lastPhrase = self.phrase
-            self.speakText()
+        self.lang = settings.get_list('parrot', 'lang')[0]
+        self.lastPhrase = self.phrase
+        self.speakText()
 
 
     def poll(self):
